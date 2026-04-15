9ea3423b043d3c6bf52a66141a027f966de8efbe
Mradul2k04 <mradul.btech.cs22@gamil.com>
2026-04-15 23:11:59 +0530

    Make thE Gambler Game so user can get there win and loss percentage

---
+++ Logical_Programs/gambler.py
@@ -0,0 +1,26 @@
+import random 
+
+stake=int(input("Enter name of the stake holder : "))
+goals=int(input("Enter your goals : "))
+trials=int(input("Enter number of trials : "))
+win=0
+total_bets=0
+for i in range(trials):
+    amount=stake
+    bets=0
+    while amount>0 and amount<goals:
+        bets+=1
+        if random.random()<0.5:
+            amount+=1
+        else:
+            amount-=1
+    total_bets+=bets
+    if amount==goals:
+        win+=1
+print(f"How much time Win {win}")
+print(f"The percentage of win {win/trials*100}")
+print(f"The percentage of loss {(trials-win)/trials*100}")
+        
+    
+                 
+                
--- Practice_Question/square.py
+++ Practice_Question/square.py
@@ -1,3 +1,3 @@
-num=float(input("Enter the number : "))
+num=int(input("Enter the number : "))
 sqrt_num=num **0.5
 print(f"The square root of number is {sqrt_num}")
