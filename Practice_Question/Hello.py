33a4d0e6bf5c9b5d6b4b8060d5035424e54f4b55
Mradul2k04 <mradul.btech.cs22@gamil.com>
2026-04-15 10:59:48 +0530

    Check user age and print it is vaild voter or not

---
+++ Practice_Question/Hello.py
@@ -0,0 +1,8 @@
+name=input("Enter your name : ")
+age=int(input("Enter you age : "))
+
+if age>=18:
+    print(f"Hello {name} you are a valid voter")
+
+else:
+    print("you are not a valid voter")   
