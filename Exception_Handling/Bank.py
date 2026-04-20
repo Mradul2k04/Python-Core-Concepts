2b45a33018883be3479ebc57b12e84e81e4c3c69
Mradul2k04 <mradul.btech.cs22@gamil.com>
2026-04-20 07:46:36 +0530

    [Mradul Sharma] add. Bank System By Exception Handling)

---
+++ Exception_Handling/Bank.py
@@ -0,0 +1,17 @@
+class Bank:
+    def __init__(self,balance):
+        self.balance=balance
+        
+    def  withdraw(self,amount):
+        if amount<0:
+            raise Exception("amount cannot br 0")
+        if self.balance<amount:
+            raise Exception("You have no money")
+        self.balance=self.balance-amount
+obj= Bank (10000)           
+try:
+    obj.withdraw(15000)
+except Exception as e:
+    print(e)
+else:
+    print(obj.balance)            
